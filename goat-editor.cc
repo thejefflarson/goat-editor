3d3ba503e62c488162ea0dba3810747eff474dd5
Jeff Larson <thejefflarson@gmail.com>
2016-07-21 18:51:14 -0400

    Shaders and gl vertex data.

--- goat-editor.cc
+++ goat-editor.cc
@@ -1,10 +1,40 @@
 #include <iostream>
 
+#include <inttypes.h>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static const char *vertex_shader =
+"                               \
+attribute vec3 pos;             \
+varying vec2 coord;             \
+void main(){                    \
+  coord = pos.xy;               \
+  gl_Position = vec4(pos, 0.0); \
+}";
 
-static void log_error(int error, const char* description) {
+static const char *fragment_shader =
+"                                           \
+uniform sampler2D text;                     \
+varying vec2 coord;                         \
+void main() {                               \
+  gl_FragColor = texture2D(text, coord.xy); \
+}";
+
+static const float32_t points[4][3] = {
+  {-1.0, -1.0, 0.0},
+  { 1.0, -1.0, 0.0},
+  {-1.0,  1.0, 0.0},
+  { 1.0,  1.0, 0.0}
+};
+
+static const uint8_t triangles[3][2] {
+  {0, 1, 2},
+  {2, 1, 3}
+};
+
+static void log_error(const int error, const char* description) {
   std::cerr << "Error:" << description;
 }
 
