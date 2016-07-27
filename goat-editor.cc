9a7f0b6d0c9d64aa9d5d987e9c8b1f45aa9bf066
Jeff Larson <thejefflarson@gmail.com>
2016-07-26 22:02:17 -0400

    spacing

--- goat-editor.cc
+++ goat-editor.cc
@@ -150,6 +150,7 @@ int main() {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
   while(!glfwWindowShouldClose(window)) {
     int w, h;
     // TODO: we should only alloc this on resize
