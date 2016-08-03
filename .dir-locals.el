450d608c01c6131bdceb088503253e84cf21eb61
Jeff Larson <thejefflarson@gmail.com>
2016-08-02 23:09:20 -0400

    fix dir locals includes

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode
-  (flycheck-clang-include-path . ("lib/include/"))
+  (eval . (let ((dir (file-name-directory (expand-file-name ".dir-locals.el"))))
+            (setq flycheck-clang-include-path
+                  (list (concat dir "lib/include")))))
   (flycheck-clang-language-standard . "c++1y")))
